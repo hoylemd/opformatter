fa813cfd84ca981937485a4b617bcb7e6986f073
hoylemd <hoylemd@gmail.com>
2013-08-21 14:32:50 -0400

    Initial commit of files

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Mike's Obsidian Portal Character block formatter</title>
+		<link rel="stylesheet" href="css/opformatter.css"/>
+	</head>
+	<body>
+		<h3>Insert the stat block copied from a pdf here</h3>
+		<textarea id="inputText"></textarea>
+		<div><input type="button" id="goButton" value="Convert"/></div>
+		<h3>Get the formatted markup here</h3>
+		<textArea id="outputText"></textarea>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+		<script src="js/opformatter.js"></script>
+	</body>
+</html>
---
+++ js/opformatter.js
@@ -0,0 +1,5 @@
+var OPF = function() {
+	return {
+
+	};
+}();
