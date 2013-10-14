6d434dd77977b861d78eb9eadbeb5888db1935be
Michael Hoyle <hoylemd@gmail.com>
2013-10-14 11:36:59 -0400

    added framework for processing the end blocks

--- ENV/main.py
+++ ENV/main.py
@@ -7,19 +7,23 @@ import fileinput
 if __name__ == "__main__":
     lines = []
 
+    # read the input file
     for line in fileinput.input():
         lines.append(line)
 
+    # preprocess the text
     input = InputBlock("".join(lines));
 
+    # lex the text
     f = open ("tokens.txt", "w")
     f.write(Lexer().readTokens(input.parsable))
     f.close();
 
+    # parse the text
     parser = Parser(Lexer())
-
     parser.parse(input.parsable)
 
+    # if the parse proceeded reasonably, add the preprocessed blocks
     if parser.error <> True:
         parser.character.tactics = input.tactics
 
--- ENV/preprocessor.py
+++ ENV/preprocessor.py
@@ -5,7 +5,7 @@ import re
 class InputBlock:
 
     # function to parse out the tactics
-    def ingest_tactics(self, tactics_block):
+    def ingestTactics(self, tactics_block):
         # initialize stuff
         sections = re.compile(r'\n(During Combat|Morale) ')
         self.tactics = {}
@@ -31,22 +31,92 @@ class InputBlock:
             # stage the next tactic
             found = next
 
+    # method to ingest a feats list
+    def ingestFeats (self, feats_block):
+	self.feats = []
+
+	# sanitize the string to just the feats
+	feats_string = feats_block[6:].replace('\n', ' ').rstrip()
+
+	# split the string
+	self.feats += feats_string.split(", ")
+
+	# store the feats list, and empty it properly if there aren't any
+	if (len(self.feats) == 1 and self.feats[0] == ''):
+		self.feats= [];
+
+    # method to ingest a skills list
+    def ingestSkills (self, skills_block):
+	self.skills = {}
+
+    # method to ingest a languages list
+    def ingestLanguages (self, languages_block):
+	self.languages = []
+
+    # method to ingest SQs
+    def ingestSpecialQualities (self, sq_block):
+	self.special_qualities = []
+
+    # method to ingest Combat Gear
+    def ingestCombatGear (self, combat_gear_block):
+	self.combat_gear = []
+
+    # method to ingest Other Gear
+    def ingestOtherGear (self, other_gear_block):
+	self.other_gear = []
+
+    # function to excise a string from the stored string
+    # removes it from the stored string, and returns it
+    # params:
+    #   start: string that starts the substring
+    #   end: string that ends the substring, optional
+    def exciseBlock(self, start, end=None):
+	block = ""
+
+	# find the beginning
+	start_index = self.parsable.find(start)
+        if (start_index > -1):
+	    # store the prefix in a temp variable
+            parsable = self.parsable[:start_index]
+
+	    # find the end index, if a string for it has been defined
+	    end_index = -1
+	    if end != None:
+		end_index = self.parsable.find(end)
+
+	    # store the suffix in the temp
+	    if (end_index > -1):
+                parsable += self.parsable[end_index:]
+
+	    # extract the target string
+            block = self.parsable[start_index:end_index];
+
+	    # store the cut-down string back in the object
+            self.parsable = parsable
+
+	return block
+
     def __init__ (self, raw_text):
         self.raw = raw_text
         self.parsable = raw_text
 
         # excise the tactics section
-        tactics_index = self.parsable.find("TACTICS")
-        if (tactics_index > 0):
-            stats_index = self.parsable.find("STATISTICS")
+	self.ingestTactics(self.exciseBlock("TACTICS", "STATISTICS"))
 
-            parsable = self.parsable[:tactics_index]
-            if (stats_index > 0):
-                parsable += self.parsable[stats_index:]
-            self.tactics_block = self.parsable[tactics_index:stats_index];
-            self.parsable = parsable
-            self.ingest_tactics(self.tactics_block)
-        else:
-            self.tactics_block = ""
-            self.tactics = {};
+	# excise the Feats section
+	self.ingestFeats(self.exciseBlock("Feats", "Skills"))
 
+	# excise the Skills section
+	self.ingestSkills(self.exciseBlock("Skills", "Languages"))
+
+	# excise the Languages section
+	self.ingestLanguages(self.exciseBlock("Languages", "SQ"))
+
+	# excise the Special Qualities section
+	self.ingestSpecialQualities(self.exciseBlock("SQ", "Combat Gear"))
+
+	# excise the Combat Gear section
+	self.ingestCombatGear(self.exciseBlock("Combat Gear", "; Other Gear"))
+
+	# excise the Other Gear section
+	self.ingestOtherGear(self.exciseBlock("; Other Gear"))
